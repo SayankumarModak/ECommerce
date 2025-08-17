cc76e92203edf3da4777612cc15ef8abac20df55
SayankumarModak <sayanmodak242001@gmail.com>
2025-08-17 11:02:25 +0530

    changes in the cookie setting part

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -149,7 +149,8 @@ const logoutUser = (req, res) => {
 const authMiddleware = async (req, res, next) => {
    try {
 
-      const authHeader = req.headers['Authorization'];
+      const authHeader = req.headers['authorization'];
+      console.log(authHeader)
       const token = authHeader && authHeader.split(' ')[1]
       if (!token) {
          return res.json({
